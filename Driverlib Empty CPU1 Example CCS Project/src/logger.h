452f4537f2417de58d32cca9c10a7043db0efc5f
snehakopparapu <srivalli.kopparapu@gmail.com>
2026-02-22 15:53:22 -0700

    update logger

--- Driverlib Empty CPU1 Example CCS Project/src/logger.h
+++ Driverlib Empty CPU1 Example CCS Project/src/logger.h
@@ -1,5 +1,13 @@
+#include "device.h"
+#include "driverlib.h"
+
+#include "c2000_freertos.h"
+#include "queue.h"
+
+#include <string.h>
+
 typedef struct logger_data{
     //vars
-    void *data_t;
+    uint8_t *data_t;
     int size;
 }logger_data;
