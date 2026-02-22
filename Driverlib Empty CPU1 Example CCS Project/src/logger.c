c2e2e0f208722391aa9441fae117315d16193b6d
snehakopparapu <srivalli.kopparapu@gmail.com>
2026-02-22 15:53:22 -0700

    update logger

--- Driverlib Empty CPU1 Example CCS Project/src/logger.c
+++ Driverlib Empty CPU1 Example CCS Project/src/logger.c
@@ -1,49 +1,38 @@
 //VCU Logger
 #include "logger.h"
 
-#include "device.h"
-#include "driverlib.h"
-
-#include "c2000_freertos.h"
-#include "queue.h"
-
-
 static QueueHandle_t loggerQueue;
 void init() {
     loggerQueue = xQueueCreate(10, sizeof(uint32_t));
 }
 
-
-void logger_send(){
-    logger_data data;
+void logger_send(logger_data sendData){
+    //logger_data data;
     BaseType_t loggerStatusSend;
     while(1){
-        loggerStatusSend = xQueueSend(loggerQueue, &data, pdMS_TO_TICKS(0));
-        /*
-        Recieve data, send to queue: loggerStatusSend = xQueueSend(loggerQueue, &data, pdMS_TO_TICKS(100));
-        Send for ISR?
-        */
+        loggerStatusSend = xQueueSend(loggerQueue, &sendData.data_t, pdMS_TO_TICKS(100));
+        //Send to queue, wait for max of 100ms if queue is full
+        
         //vTaskDelay(1000);
         if (loggerStatusSend != pdPASS) {
             // Failed to send (queue was full for the entire 100ms)
+            #error Failed to send to queue
         }
     }
     
 
 }
 
-void logger_receive(){
-   logger_data receivedData;
+void logger_receive(logger_data receivedData){
+   //logger_data receivedData;
    BaseType_t loggerStatusReceive;
    
     while (1){
-        loggerStatusReceive = xQueueReceive(loggerQueue, &receivedData, portMAX_DELAY);
-        /*
-        Receive data from queue: loggerStatusReceive = xQueueReceive(loggerQueue, &receivedData, portMAX_DELAY);
-        Receive from ISR?
-        */
+        loggerStatusReceive = xQueueReceive(loggerQueue, &receivedData.data_t, portMAX_DELAY);
+        //Receive data from queue, wait indefinitely until data is available
+        
         if (loggerStatusReceive != pdPASS) {
-            // Failed to send (queue was full for the entire 100ms)
+            #error Failed to Receive to queue
         }
     }
 }
