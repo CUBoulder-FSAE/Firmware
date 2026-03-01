66dadbb3678709f4d1703b86c416d84b5e7b7054
snehakopparapu <srivalli.kopparapu@gmail.com>
2026-03-01 11:51:47 -0700

    in progress

--- Driverlib Empty CPU1 Example CCS Project/src/logger.h
+++ Driverlib Empty CPU1 Example CCS Project/src/logger.h
@@ -6,8 +6,18 @@
 
 #include <string.h>
 
+#define LOGGER_QUEUE_SIZE 10u
+#define LOGGER_WAIT_TO_SEND 100u
+
 typedef struct logger_data{
     //vars
     uint8_t *data_t;
     int size;
-};
+} logger_data;
+
+//Initializes queue
+void init();
+
+void logger_send(logger_data);
+
+void logger_receive(logger_data);
