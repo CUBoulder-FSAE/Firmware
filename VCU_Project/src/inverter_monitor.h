5ffed9b258c35411e41028031ca76d2e5368b810
Dylan Bomgardner <dylan.m.bomgardner@gmail.com>
2026-03-02 20:09:08 -0700

    comments

--- VCU_Project/src/inverter_monitor.h
+++ VCU_Project/src/inverter_monitor.h
@@ -1,6 +1,13 @@
 #include "driverlib.h"
 #include "device.h"
 
+// create can message object that can read all inverter transmit values
+// create task 
+// create queue
 void inverter_task_init(void);
 
+
+// read from CAN message queue
+// depending on ID of the message parse for important information
+// set the static variables to keep track of important things (is motor spinning, rpm of motor, )
 void inverter_task_loop(void*);
