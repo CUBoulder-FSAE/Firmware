22712d38eeea42f59b19824e2a76ceedeead15f0
Matveybu <bubalo.matvey@gmail.com>
2026-03-05 19:21:11 -0700

    deleteing a folder

--- Driverlib Empty CPU1 Example CCS Project/src/vcu_adc.c
+++
@@ -1,7 +0,0 @@
-#include "vcu_adc.h"
-#include "driverlib.h"
-#include "device.h"
-#include "board.h"
-
-#define VCU_ADC_REFERENCE_VOLTAGE 2.5f
-#define VCU_ADC_RESOLUTION 4095 //12-bit ADC maximum value
--- Driverlib Empty CPU1 Example CCS Project/src/vcu_adc.h
+++
@@ -1,8 +0,0 @@
-#ifndef VCU_ADC_H
-#define VCU_ADC_H
-
-void VCU_ADC_Init(void);
-uint16_t VCU_ADC_ReadRaw(void);
-float VCU_ADC_ReadVoltage(void);
-
-#endif
