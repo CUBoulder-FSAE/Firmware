a308062ba8055f709887e3b8a22786592269ea87
Dylan Bomgardner <dylan.m.bomgardner@gmail.com>
2026-03-07 15:11:53 -0700

    added comments and called init in main

--- VCU_Project/src/inverter_transmit.h
+++ VCU_Project/src/inverter_transmit.h
@@ -1,8 +1,18 @@
 #pragma once
 #include "driverlib.h"
 
+/**
+ * Initilaize CAN Message Objects
+ */
 void inverter_transmit_init();
 
+/**
+ * Set requested current value from the inverter
+ * @param current 0-200
+ */
 void inverter_transmit_set_current(uint8_t current);
 
+/**
+ * Set drive enable to 1
+ */
 void inverter_transmit_drive_enable();
